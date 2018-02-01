3e75f21eb5996d7b6f7db87ebfe7e870db849c78
jcalderi <jcalderi@arbfile.org>
2018-02-01 15:30:13 -0500

    Adding toast message

--- src/providers/cubicles/cubicles.ts
+++ src/providers/cubicles/cubicles.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import {AngularFireDatabase} from "angularfire2/database";
+import {AngularFireDatabase, AngularFireObject } from "angularfire2/database";
 import {Observable} from "rxjs/Observable";
 import {LoginAndRegistrationProvider} from "../../providers/login-and-registration/login-and-registration";
+import {MessagingProvider} from "../../providers/messaging/messaging";
 import { CRSCubicle } from './../../models/CRSCubicle';
 
 /*
@@ -15,7 +16,7 @@ export class CubiclesProvider {
 
   cubicles: CRSCubicle[];
 
-  constructor(private afDatabase: AngularFireDatabase, private lrService: LoginAndRegistrationProvider) {
+  constructor(private afDatabase: AngularFireDatabase, private lrService: LoginAndRegistrationProvider, private messagingService: MessagingProvider ) {
     console.log('Hello CubiclesProvider Provider');
   }
 
@@ -31,10 +32,12 @@ export class CubiclesProvider {
   reserveCubicle(id: number) {
     console.log("reserving cubicle " + id + " for " + this.lrService.user.name);
     this.afDatabase.database.ref('cubicles/'+id).update({person: this.lrService.user.uid, personName: this.lrService.user.name});
+    this.messagingService.toast("You are all set. We have secured your space!");
   }
 
   releaseCubicle(id: number) {
     this.afDatabase.database.ref('cubicles/'+id).update({person: null, personName: null});
+    this.messagingService.toast("Thank you for releasing this space");
   }
 
 }
