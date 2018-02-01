166acb68004591d0e935d6bfbee545c2e1b60630
jcalderi <jcalderi@arbfile.org>
2018-02-01 07:48:13 -0500

    Deleting Cubicle model and moving fields to CRSCubicle

--- src/models/CRSCubicle.ts
+++ src/models/CRSCubicle.ts
@@ -2,7 +2,10 @@
  * Created by robpassante on 1/26/18.
  */
 export class CRSCubicle{
-
+  public id: number;
+  public permanent: boolean;
+  public team: string;
+  public person: string;
 
   constructor(){}
 
--- src/models/Cubicle.ts
+++
@@ -1,9 +0,0 @@
-export class Cubicle{
-  public id: number;
-  public permanent: boolean;
-  public team: string;
-  public person: string;
-
-  constructor(){}
-
-}
