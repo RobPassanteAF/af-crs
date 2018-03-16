16fdd3b2a963905a80a3adb1965e71e53a9f5e9f
jcalderi <jcalderi@arbfile.org>
2018-03-16 16:30:05 -0400

    Removing ID from Cubicle object

--- src/models/CRSCubicle.ts
+++ src/models/CRSCubicle.ts
@@ -2,7 +2,7 @@
  * Created by robpassante on 1/26/18.
  */
 export class CRSCubicle{
-  public id: number;
+  // public id: number;
   public permanent: boolean;
   public team: string;
   public person: string;
