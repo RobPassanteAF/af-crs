2a02087de689c5388ced41ebf7033504dd895cca
jcalderi <jcalderi@arbfile.org>
2018-03-19 20:33:22 -0400

    Changing header

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {MessagingProvider} from "../../providers/messaging/messaging";
 
 @Component({
   selector: 'page-home',
@@ -7,12 +8,12 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,  private messagingService: MessagingProvider) {
 
   }
 
   selectLocation(location: string) {
-    alert("You are at " + location);
+    this.messagingService.toast("You are at " + location);
   }
 
 }
