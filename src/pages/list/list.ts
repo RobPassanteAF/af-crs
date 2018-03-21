9bac0688260b4be5267a6b6bd4048b468552399a
jcalderi <jcalderi@arbfile.org>
2018-03-21 10:19:33 -0400

    Changes to show camera preview when using QR Scanner

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -82,28 +82,28 @@ export class ListPage {
     this.qrScanner.prepare()
   .then((status: QRScannerStatus) => {
      if (status.authorized) {
-       // camera permission was granted
+       
+      (window.document.querySelector('ion-app') as HTMLElement).classList.add('cameraView');
+      (window.document.querySelector('.scroll-content') as HTMLElement).classList.add('hideScrollContent');
 
-
-       // start scanning
-       let scanSub = this.qrScanner.scan().subscribe((text: string) => {
-         console.log('Scanned something', text);
+       let scanSub = this.qrScanner.scan().subscribe((scannedCubicleId: string) => { 
+        console.log('Scanned something', scannedCubicleId);
+        
+         this.reserveCubicle(Number(scannedCubicleId));
 
          this.qrScanner.hide(); // hide camera preview
+         (window.document.querySelector('ion-app') as HTMLElement).classList.remove('cameraView');
+         (window.document.querySelector('.scroll-content') as HTMLElement).classList.remove('hideScrollContent');
          scanSub.unsubscribe(); // stop scanning
        });
 
        // show camera preview
        this.qrScanner.show();
 
-       // wait for user to scan something, then the observable callback will be called
-
      } else if (status.denied) {
-       // camera permission was permanently denied
-       // you must use QRScanner.openSettings() method to guide the user to the settings page
-       // then they can grant the permission from there
+       console.log('permision denied');
      } else {
-       // permission was denied, but not permanently. You can ask for permission again at a later time.
+      console.log('permision denied');    // DO something over here to alert the user
      }
   })
   .catch((e: any) => console.log('Error is', e));
