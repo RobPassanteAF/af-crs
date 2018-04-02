337e7c44c28842c041cd1e8b7dfebfdf806b826e
jcalderi <jcalderi@arbfile.org>
2018-04-02 15:51:53 -0400

    Keeping track of empty cubicles when releasing and reserving

--- src/providers/cubicles/cubicles.ts
+++ src/providers/cubicles/cubicles.ts
@@ -15,9 +15,12 @@ import {AngularFireAuth} from "angularfire2/auth";
 export class CubiclesProvider {
 
   cubicles: CRSCubicle[];
+  emptyCubicles: number;
 
   constructor(private afDatabase: AngularFireDatabase, private afAuth:AngularFireAuth, private lrService: LoginAndRegistrationProvider ) {
     console.log('Hello CubiclesProvider Provider');
+    this.getEmptyCubicles().subscribe(val => this.emptyCubicles = val);
+    
   }
 
   getAllCubibles() :Observable<CRSCubicle[]> {
@@ -25,22 +28,41 @@ export class CubiclesProvider {
 
   }
 
+  getEmptyCubicles() :Observable<any> {
+    return this.afDatabase.object('/emptyCubicles').valueChanges();   
+  }
+
   reserveCubicle(id: number) {
     console.log('reserving ' + id );
-    this.releaseCubicle(this.lrService.user.cubicle);
+    if(this.lrService.user.cubicle) {
+      this.afDatabase.database.ref('/').update({emptyCubicles:this.emptyCubicles-1});
+    }
+    
+    this.releaseCubicle(this.lrService.user.cubicle, false);
+
 
     this.afDatabase.database.ref('cubicles/'+id).update({person: this.lrService.user.uid, personName: this.lrService.user.name});
     let peopleURI = "people/" + this.afAuth.auth.currentUser.uid;
     this.lrService.user.cubicle = id;    
     this.afDatabase.database.ref().child(peopleURI).update({cubicle: this.lrService.user.cubicle});
+    
   }
 
-  releaseCubicle(id: number) {
+  releaseCubicle(id: number, releasing = true ) {
+    if(releasing) {
+      this.afDatabase.database.ref('/').update({emptyCubicles:this.emptyCubicles+1});
+    }
+    
     console.log('releasing ' + id );
     this.afDatabase.database.ref('cubicles/'+id).update({person: null, personName: null});
+    // this.refreshEmptyCubicles();
     this.lrService.user.cubicle = null;    // TODO: this will work once firebase async is connected
     let peopleURI = "people/" + this.afAuth.auth.currentUser.uid;
     this.afDatabase.database.ref().child(peopleURI).update({cubicle: this.lrService.user.cubicle});
+  }
+
+  private refreshEmptyCubicles() {
+    
   }
 
 
