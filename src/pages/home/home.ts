7a95b58426b2a050e64cdb24a8ef07fdc601e7db
jcalderi <jcalderi@arbfile.org>
2018-08-09 10:23:44 -0400

    Adding temporary workaround to release all cubicles for each day

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,6 +4,8 @@ import {MessagingProvider} from "../../providers/messaging/messaging";
 import { CubiclesProvider } from '../../providers/cubicles/cubicles';
 import { TeamsProvider } from '../../providers/teams/teams';
 import { ReservePage } from '../reserve/reserve';
+import { CRSCubicle } from "../../models/CRSCubicle";
+import { Observable } from "rxjs/Observable";
 
 @Component({
   selector: 'page-home',
@@ -12,6 +14,7 @@ import { ReservePage } from '../reserve/reserve';
 export class HomePage implements OnDestroy {
   emptyCubicles: number;
   myTeamsAndTeammates;
+  cubicles: Observable<CRSCubicle[]>;
 
   constructor(public navCtrl: NavController,  private messagingService: MessagingProvider, private cubiclesService: CubiclesProvider, private     teamsService: TeamsProvider) {
     this.cubiclesService.getEmptyCubicles().subscribe( (emptyCubes)=> {
@@ -19,16 +22,26 @@ export class HomePage implements OnDestroy {
       this.emptyCubicles = emptyCubes;
     });
 
-    this.cubiclesService.getLastReleaseAllDate().subscribe( (lastReleaseAllDate)=> {
-      console.log(lastReleaseAllDate);
-      var scheduledRelease = new Date();
-      scheduledRelease.setHours(5,0,0,0);
-      if(new Date(lastReleaseAllDate).valueOf() < scheduledRelease.valueOf()) {
-        console.log('setting date as now -> ' + new Date());
-        this.cubiclesService.setLastReleaseAllDate(new Date());
-      }
-    });
+    this.releaseAllCubicles();    // temporary workaround to release all cubicles 
     this.getMyTeammates();
+  }
+
+  releaseAllCubicles() {
+    this.cubiclesService.getAllCubibles().subscribe(cubicles => {
+      this.cubiclesService.getLastReleaseAllDate().subscribe( (lastReleaseAllDate)=> {
+        console.log(lastReleaseAllDate);
+        var scheduledRelease = new Date();
+        scheduledRelease.setHours(5,0,0,0);
+        if(new Date(lastReleaseAllDate).valueOf() < scheduledRelease.valueOf()) {
+          cubicles.filter( cubicle => cubicle.person != null).forEach(cubicle => {
+            console.log('About to automatically release ' + cubicle.cubeId);
+            this.cubiclesService.releaseCubicle(cubicle.cubeId);
+          });
+          console.log('setting date as now -> ' + new Date());
+          this.cubiclesService.setLastReleaseAllDate(new Date());
+        }
+      });
+    });
   }
 
   ngOnDestroy(){
